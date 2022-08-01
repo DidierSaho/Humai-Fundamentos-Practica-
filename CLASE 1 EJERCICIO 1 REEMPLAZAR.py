8727f798484dc1fe0cf7c7d4630356cfdefdb5b9
DidierSaho <95487810+DidierSaho@users.noreply.github.com>
2022-08-01 20:17:34 -0300

    ejercicio de reemplazar

---
+++ CLASE 1 EJERCICIO 1 REEMPLAZAR.py
@@ -0,0 +1,5 @@
+string = "Sinopsis | Marty Mcfly, es un tipico adolcente americando de los años ochenta, es enviado accidentalmente de vuelta a 1944 en una maquina del tiempo realizada con un Delorean inventada por un cientifico un poco loco. | En este viaje, Marty debe asegurarse de que sus padres se encuentren y se enamoren, para que pueda volver a su tiempo."
+
+
+nuevo = string.replace("|","\n")
+print(nuevo)
